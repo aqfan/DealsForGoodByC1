4ccbb910b00a776483805ea7ec95a9724ea6ed69
Bilguunzaya Battogtokh <bbatt99@stanford.edu>
2018-05-24 16:46:15 -0400

    Created api

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from app import api
 import requests
 from random import choice
 from string import ascii_uppercase
@@ -6,13 +7,14 @@ import json
 from decimal import Decimal
 from flask import render_template
 
+
 apiKey = '6c8ac0ed9c3bdb249bbe32d61526f18a'
 
 dealInfo = { # mapping of ids to info about the deal
     'homeDepotWWF' : {
         'merchantID' : 'homeDepot',
         'charityNessieID' : '5b06fa43f0cec56abfa40c84',
-        'charityName' = 'World Wildlife Fund',
+        'charityName' : 'World Wildlife Fund',
         'amount' : Decimal(10.00) ,
         'title' : '20% off Home Depot',
         'description' : 'longer description of the deal',
@@ -29,30 +31,27 @@ def dealpage(id):
         return '404!!!!!!!!!!!!!!!!! this deal dont exist'
 
 
-
-
-
 @app.route('/')
 def homepage():
-	return render_template
+	return 'various deals are provided here'
 
-@app.route('/merchants')
-def merchantpage():
-	return 'the merchant pages are linked to here'
-
-@app.route('/charities')
-def charitypage():
-	return 'the charity pages are linked to here'
-
-#TODO: /merchants/<id>
-"""
-describes the merchant and links the deals offered by the merchant
-"""
-
-#TODO: /charities/<id>
-"""
-describes the charity and links the deals associated with the charity
-"""
+# @app.route('/deals)
+# def merchantpage():
+# 	return 'the merchant pages are linked to here'
+#
+# @app.route('/charities')
+# def charitypage():
+# 	return 'the charity pages are linked to here'
+#
+# #TODO: /merchants/<id>
+# """
+# describes the merchant and links the deals offered by the merchant
+# """
+#
+# #TODO: /charities/<id>
+# """
+# describes the charity and links the deals associated with the charity
+# """
 
 if __name__ == '__main__':
 	app.run(debug=True)

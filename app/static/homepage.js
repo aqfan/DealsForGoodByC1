6ccda254c6d8cd5fdf7ea9e4693cce18488e6edb
aqfan <angelf@seas.upenn.edu>
2018-05-24 23:58:30 -0400

    homepage.js

--- app/static/homepage.js
+++ app/static/homepage.js
@@ -3,16 +3,24 @@ var app = angular.module("homepage", []);
 app.controller("ListController", function($scope, $http) {
   $scope.iconGroup = [];
 
+  // $.ajax({
+  //     type: "GET",
+  //     dataType: 'json',
+  //     url: "http://deals-api.herokuapp.com/deals/",
+  //     crossDomain : true
+  // })
+  //     .done(function( data ) {
+  //         console.log(data);
+  //     });
+
   var req = new XMLHttpRequest();
   req.responseType = "json";
-  req.open("GET", "http://deals-api.herokuapp.com/deals/" + localStorage['store_name'], true);
+  req.open("GET", "http://deals-api.herokuapp.com/deals/", true);
   req.onreadystatechange = function() {
     if (req.readyState == 4) {
       if (req.status == 200) {
         var companyInfo = req.response;
         console.log(companyInfo);
-        $('#charity_img').attr('src',companyInfo["image-link"]);
-        $('#percentage').text(companyInfo.percent_off);
       }
     }
   };
